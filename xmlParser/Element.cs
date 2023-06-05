eca303feacb845cf6c757772fe41a842ebb89de0
Ilia <pops375@yandex.ru>
2023-06-05 17:51:42 +0300

    Добавьте файлы проекта.

---
+++ xmlParser/Element.cs
@@ -0,0 +1,93 @@
+﻿using System.Text.RegularExpressions;
+
+namespace xmlParser
+{
+    public class Element
+    {
+        public string name;
+        public string content;
+        public List<Element> children = new List<Element>();
+        public Dictionary<string, string> attributes = new Dictionary<string, string>();
+
+
+        public Element(string name, string content, string? attributes = null)
+        {
+            this.content = content;
+            this.name = name;
+
+            CreateElement();
+            CalculateAttributes(attributes);
+        }
+
+        public void CalculateAttributes(string? attributes)
+        {
+            if (attributes != null && attributes.Contains("="))
+            {
+                foreach (var item in attributes.Split(" "))
+                {
+                    if (item.Contains("="))
+                    {
+                        var pair = item.Split("=");
+                        this.attributes.Add(pair.FirstOrDefault(), pair.LastOrDefault());
+                    }
+                }
+            }
+        }
+
+        public int FindReplicaOpenTag(string tagName, string content)
+        {
+            Regex replicaOpenTeg = new Regex(@"<" + tagName + @"\b[^>]*");
+            var replicaOpenTags = replicaOpenTeg.Matches(content);
+
+            return replicaOpenTags.Count;
+        }
+
+        public void CreateElement()
+        {
+            Regex regexOpenTag = new Regex(@"<([^>]+)>");
+            var openTag = regexOpenTag.Match(content);
+
+            if (openTag.Value == "")
+            {
+
+                return;
+            }
+
+            var tagName = openTag.Value.Trim().Trim('<', '>');
+
+            var attributes = tagName.Contains(' ') ? tagName.Substring(tagName.IndexOf(' ')).Trim() : null;
+            tagName = tagName.Split(" ")[0].Trim();
+
+            Regex regexCloseTag = new Regex(@"<\/" + tagName + ">");
+
+            var closeTag = regexCloseTag.Matches(content);
+
+            var start = openTag.Index + openTag.Length;
+            var length = closeTag[0].Index - openTag.Length + openTag.Index;
+            var tagContent = content.Substring(start, length).Trim();
+
+            var replicaOpen = FindReplicaOpenTag(tagName, tagContent);
+
+            if (replicaOpen > 0)
+            {
+                start = openTag.Index + openTag.Length;
+                length = closeTag[replicaOpen].Index - openTag.Length + openTag.Index;
+                tagContent = content.Substring(start, length).Trim();
+            }
+
+            children.Add(new Element(tagName, tagContent, attributes: attributes));
+
+            if (closeTag[replicaOpen].Index + closeTag[replicaOpen].Length != content.Length)
+            {
+                content = content.Substring(closeTag[replicaOpen].Index + closeTag[replicaOpen].Length).Trim();
+                CreateElement();
+            }
+        }
+
+        public override string ToString()
+        {
+
+            return name;
+        }
+    }
+}
---
+++ xmlParser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace xmlParser
+{
+    public class Program
+    {
+        public static async Task<string> LoadText(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[fs.Length];
+                await fs.ReadAsync(buffer, 0, buffer.Length);
+                string textFromFile = Encoding.Default.GetString(buffer);
+                return textFromFile;
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            var textPath = "test.xml";
+            var text = await LoadText(textPath);
+
+            var doc = new Element("document", text);
+        }
+    }
+}
